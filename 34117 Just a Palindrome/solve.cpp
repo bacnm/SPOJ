ea9da511c87e05f972b5cbd48a412f8b66c81a58
DragoN <bac.nguyenminh260@gmail.com>
2022-06-27 17:36:30 +0700

    Update

---
+++ 34117 Just a Palindrome/solve.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+
+
+
+int main()
+{
+    freopen("input.txt","r",stdin);
+    int tcs = 0;
+    cin >> tcs;
+
+
+
+    for (int tc = 0; tc < tcs;tc++)
+    {
+        
+    }
+    return 0;
+}
